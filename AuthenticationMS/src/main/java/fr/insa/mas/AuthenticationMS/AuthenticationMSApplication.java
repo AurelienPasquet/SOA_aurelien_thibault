4869f7e86bc4024233510444523adf5b29ac5d8e
ThibaultGautheron <tgauther@insa-toulouse.fr>
2024-11-26 10:11:34 +0100

    auth

---
+++ AuthenticationMS/src/main/java/fr/insa/mas/AuthenticationMS/AuthenticationMSApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.mas.AuthenticationMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationMSApplication.class, args);
+	}
+
+}
